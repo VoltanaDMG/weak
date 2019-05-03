6bac7d2871f6b1b6654a096bc555c18e1952331d
VoltanaDMG <50107216+VoltanaDMG@users.noreply.github.com>
2019-05-03 15:39:00 +0200

    [~](Updated) `index.js`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
-const bindings = require('bindings);
+const bindings = require("bindings");
 
 // Use the native bindings of the weak-napi package directly
 module.exports = bindings({
