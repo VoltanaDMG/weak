56ba53bae93f80641b5e78cea396dc5e0f72782b
VoltanaDMG <50107216+VoltanaDMG@users.noreply.github.com>
2019-05-03 15:11:43 +0200

    [>][ci skip](Initialization) Started creation of a native module

---
+++ index.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) No one - this is OSS. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+const bindings = require('bindings);
+
+// Use the native bindings of the weak-napi package directly
+module.exports = bindings({
+  bindings: "weakref.node",
+  module_root: bindings.getRoot(require("./")),
+});
