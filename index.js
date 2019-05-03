41d102a3cf3a420c3068d461eba45cc5abc800aa
VoltanaDMG <50107216+VoltanaDMG@users.noreply.github.com>
2019-05-03 16:07:06 +0200

    [~](Updated) `index.js`

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const bindings = require("bindings");
 // Use the native bindings of the weak-napi package directly
 module.exports = bindings({
   bindings: "weakref.node",
-  module_root: bindings.getRoot(require.resolve("./")),
+  module_root: bindings.getRoot(require.resolve("./index.js")),
 });
